e39ac6d5315c95f9e20bc602f35c6554eaa15757
MariaJuliaCosta <103979116+MariaJuliaCosta@users.noreply.github.com>
2023-03-21 01:32:20 -0300

    Update character.dart

--- lib/model/character.dart
+++ lib/model/character.dart
@@ -1,3 +1,36 @@
+class DataMarvel {
+  DataMarvel({
+    required this.offset,
+    required this.limit,
+    required this.total,
+    required this.count,
+    required this.results,
+  });
+
+  int offset;
+  int limit;
+  int total;
+  int count;
+  List<CharactersMarvel> results;
+
+  factory DataMarvel.fromJson(Map<String, dynamic> json) => DataMarvel(
+        offset: json["offset"],
+        limit: json["limit"],
+        total: json["total"],
+        count: json["count"],
+        results: List<CharactersMarvel>.from(
+            json["results"].map((x) => CharactersMarvel.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "offset": offset,
+        "limit": limit,
+        "total": total,
+        "count": count,
+        "results": List<dynamic>.from(results.map((x) => x.toJson())),
+      };
+}
+
 class CharactersMarvel {
   CharactersMarvel({
     required this.name,
